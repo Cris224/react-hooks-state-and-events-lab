9cfb9bdcd73e2d0a329c7a4319b5fdcf86b5cfb7
Cristofer <cristoferlorenzana@gmail.com>
2021-11-07 22:10:35 -0600

    commit tree 4276451427fdf3ca59f82921dde1702b8a28af95

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [cart, setCart] = useState(false)
+
+  function toggleCart(){
+    setCart(cart => !cart);
+  }
+
+  const inCart = cart ? "in-cart" : "";
+  const btnClr = cart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={btnClr} onClick={toggleCart}>{cart ? "Remove From Cart" : "Add to Cart" }</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,18 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function filterCategory(items){
+    const newItemList = items.filter((food) => food.category !== selectedCategory.value)
+    setSelectedCategory(newItemList);
+    }
+  
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
+      <div  className="Filter">
+        <select onChange={() => filterCategory} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
-      <ul className="Items">
+      <ul className="Items" id="items-list">
         {items.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
